83b3261ccc7e617fcd6cc3e524626f00113ca9ec
Yousif Ahmed <56611494+Yousif-Ahmed@users.noreply.github.com>
2021-12-21 13:42:43 +0200

    reg file finalized

--- RTL Code/reg_file/RegFile.vhd
+++ RTL Code/reg_file/RegFile.vhd
@@ -9,7 +9,6 @@ ENTITY RegFile IS
        Rsrc1_address    : IN     STD_LOGIC_VECTOR (2  DOWNTO 0) ; -- 3bit
        Rsrc2_address    : IN     STD_LOGIC_VECTOR (2  DOWNTO 0) ; -- 3bit
        Rdst_address     : IN     STD_LOGIC_VECTOR (2  DOWNTO 0) ; -- 3bit
-       enable_read_decode    : IN     STD_LOGIC ;
 
        write_back_enable: IN     STD_LOGIC ;                  
        write_address    : IN     STD_LOGIC_VECTOR (2  DOWNTO 0); --3bits input 
@@ -66,9 +65,10 @@ SIGNAL enb_write_address: STD_LOGIC_VECTOR (7 DOWNTO 0);
 -- regester out
 type reg_output is array (0 to 7) of STD_LOGIC_VECTOR (15 DOWNTO 0) ;
 SIGNAL r_FF_OUT : reg_output ;
+SIGNAL enable_read_decode : STD_LOGIC ;
 
 BEGIN
-
+enable_read_decode <= '1' ;
 --Source decoders for reading from registers
 src1 :  Decoder PORT MAP (Rsrc1_address ,enable_read_decode  , enb_Rsrc1_address);
 src2 :  Decoder PORT MAP (Rsrc2_address ,enable_read_decode  , enb_Rsrc2_address);
