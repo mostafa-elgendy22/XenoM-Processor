3cfef18831b52d6d4fffbec2b3367fb37ed992d3
Mostafa wael <mostafa.w.k000@gmail.com>
2021-12-22 18:12:19 +0200

    partially finished the intergration

--- RTL Code/decode_stage/decoding_stage.vhd
+++ RTL Code/decode_stage/decoding_stage.vhd
@@ -11,10 +11,7 @@ entity decoding_stage is
       write_address               : IN STD_LOGIC_VECTOR (2  DOWNTO 0); --3bits input 
       write_data                  : IN STD_LOGIC_VECTOR (15 DOWNTO 0); 
       --------------------------- outputs ---------------------------
-  
-      ---- Related to the immediate data
-      immediate_data              : out std_logic_vector(15 downto 0);
-  
+      -- TODO: add immediate_data
       ---- Related to the reg file and the operands
       write_back_enable_out       : out std_logic; -- if the instruction need to write back
       
@@ -39,23 +36,35 @@ entity decoding_stage is
       is_hlt_instruction          : out std_logic ;
      
       --- reg file output 
-      Rsrc1_data                  : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) ;   
-      Rsrc2_data                  : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) ;  
-      Rdst_data                   : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0)  
-    
+      -- Rsrc1_data                  : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) ;   
+      -- Rsrc2_data                  : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) ;  
+      -- Rdst_data                   : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) 
+      operand1 : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) ;   
+      operand2 : OUT    STD_LOGIC_VECTOR (15 DOWNTO 0) 
   
     );
   end entity;
     
 
   architecture a_decoding_stage OF decoding_stage is 
+    SIGNAL  Rsrc1_data                  : STD_LOGIC_VECTOR (15 DOWNTO 0);   
+    SIGNAL  Rsrc2_data                  : STD_LOGIC_VECTOR (15 DOWNTO 0);  
+    SIGNAL  Rdst_data                   : STD_LOGIC_VECTOR (15 DOWNTO 0);
+    SIGNAL  immediate_data              : STD_LOGIC_VECTOR (15 DOWNTO 0);
+
     SIGNAL Rsrc1_address : STD_LOGIC_VECTOR (2 DOWNTO 0);
     SIGNAL Rsrc2_address : STD_LOGIC_VECTOR (2 DOWNTO 0);
     SIGNAL Rdst_address : STD_LOGIC_VECTOR (2 DOWNTO 0);
     
     SIGNAL is_immediate :STD_LOGIC ;
     SIGNAL is_operation_on_Rdst :STD_LOGIC ;
+
     BEGIN 
+
+    operand1 <= Rdst_data when is_operation_on_Rdst = '1' else Rsrc1_data;
+    operand2 <= immediate_data when is_immediate = '1' else Rsrc2_data;
+
+
     Control_unit :  ENTITY work.control_unit 
     port MAP  (    
                     instruction   => instruction ,
