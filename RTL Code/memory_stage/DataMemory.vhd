b8f262b8e25357e2437370a3f9649fb1c219a832
Yousif-Ahmed <yousif.lasheen60@gmail.com>
2022-01-07 01:29:18 +0200

    update data memory

--- RTL Code/memory_stage/DataMemory.vhd
+++ RTL Code/memory_stage/DataMemory.vhd
@@ -2,15 +2,16 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.numeric_std.ALL;
 USE IEEE.math_real.ALL;
-use IEEE.std_logic_unsigned.all;
+USE IEEE.std_logic_unsigned.ALL;
 
 ENTITY data_memory IS
-       PORT (
-              address : IN STD_LOGIC_VECTOR(19 DOWNTO 0); --20bit memory address from execution unit 
-              datain  : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- 32 data input    
-              memory_read: IN STD_LOGIC;                -- signal read from memory with address
-              memory_write:IN STD_LOGIC_Vector(1 DOWNTO 0); --signal write in memory
-              dataout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0)); 
+   PORT (
+      clk : IN STD_LOGIC;
+      address : IN STD_LOGIC_VECTOR(19 DOWNTO 0); --20bit memory address from execution unit 
+      datain : IN STD_LOGIC_VECTOR(31 DOWNTO 0); -- 32 data input    
+      memory_read : IN STD_LOGIC; -- signal read from memory with address
+      memory_write : IN STD_LOGIC_VECTOR(1 DOWNTO 0); --signal write in memory
+      dataout : OUT STD_LOGIC_VECTOR(15 DOWNTO 0));
 END ENTITY;
 
 --memory write 2 bits
@@ -21,16 +22,26 @@ END ENTITY;
 -- 1|1  32 bit data 
 ARCHITECTURE a_data_memory OF data_memory IS
 
-       TYPE ram_type IS ARRAY(0 TO (2 ** 20) - 1) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
-       SIGNAL ram : ram_type;
+   TYPE memory_type IS ARRAY(0 TO (2 ** 20) - 1) OF STD_LOGIC_VECTOR(15 DOWNTO 0);
+   SIGNAL ram : memory_type;
 
 BEGIN
-    -- data out from memory 
-       dataout(15 DOWNTO 0) <= ram(to_integer(unsigned(address))) WHEN memory_read = '1' 
-       Else (others => 'Z');
-    -- data write In memory 16 bit data 
-       ram(to_integer(unsigned(address))) <= datain(31 DOWNTO 16) WHEN memory_write(0) = '1'; 
-    -- data write In memory 32 bit data 
-    ram(to_integer(unsigned(address+1))) <= datain(15 DOWNTO 0) WHEN memory_write(1) = '1' ; 
+   PROCESS (clk) IS
+   BEGIN
+      IF rising_edge(clk) THEN
+         -- data write In memory 16 bit data 
+         IF memory_write(0) = '1' THEN
+            ram(to_integer(unsigned(address))) <= datain(31 DOWNTO 16);
+         END IF;
+         -- data write In memory 32 bit data 
+         IF memory_write(1) = '1' THEN
+            ram(to_integer(unsigned(address + 1))) <= datain(15 DOWNTO 0);
+         END IF;
 
+      END IF;
+   END PROCESS;
+   -- data out from memory 
+   dataout(15 DOWNTO 0) <= ram(to_integer(unsigned(address))) WHEN memory_read = '1'
+ELSE
+   (OTHERS => 'Z');
 END ARCHITECTURE;
