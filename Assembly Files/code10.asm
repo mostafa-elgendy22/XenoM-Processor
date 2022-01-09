c298f63ea0b84af099b13f80cbc971268e569cbf
Mostafa wael <mostafa.w.k000@gmail.com>
2022-01-09 08:43:14 +0200

    add code 10

---
+++ Assembly Files/code10.asm
@@ -0,0 +1,33 @@
+# all numbers in hex format
+# we always start by reset signal
+# this is a commented line
+.ORG 0  #this means the the following line would be  at address  0 , and this is the reset address
+FF
+#you should ignore empty lines
+
+.ORG 2  #this is empty stack exception handler address
+100
+
+.ORG 4  #this is invalid addess exception handler address
+150
+
+.ORG 6  #this is int 0
+200
+
+.ORG 8  #this is int 2
+250
+
+.ORG FF
+LDM R1 ,5 ;        #add 5 in R1
+LDM R2 ,19 ;       #add 19 in R2
+LDM R3 , FFFF ;      #FFFF
+LDM R4 , F320;       #F320
+MOV R3,R5    #R5 = FFFF , flags no change
+ADD R4,R1,R4    #R4= F325 , C-->0, N-->1, Z-->0
+NOP ;
+NOP ;
+SUB R6,R5,R4    #R6= 0CDA , C-->0, N-->0,Z-->0 here carry is implemented as borrow you can implement it as not borrow
+AND R4,R7,R4    #R4= 0000 , C-->no change, N-->0, Z-->1
+IADD R2,R2,FFFF #R2= 0018 (C = 1,N,Z= 0)
+NOP ;
+ADD R2,R1,R2    #R2= 001D (C,N,Z= 0)
--- RTL Code/processor.do
+++ RTL Code/processor.do
@@ -1,7 +1,7 @@
 vsim -gui work.processor
 
 # change the name of memory file in the following command
-mem load -i {../../Memory Files/mem9.mem} /processor/fetch/instruction_memory/ram
+mem load -i {../../Memory Files/mem10.mem} /processor/fetch/instruction_memory/ram
 
 add wave -position insertpoint  \
 sim:/processor/clk \
